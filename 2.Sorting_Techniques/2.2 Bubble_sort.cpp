4b1cb9601d6530d35f3b3d152151b795ba021f7b
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2022-09-02 00:00:18 +0530

    Create 2.2 Bubble_sort.cpp

---
+++ 2.Sorting_Techniques/2.2 Bubble_sort.cpp
@@ -0,0 +1,16 @@
+//Time Complexity: O(n^2), (nested for loop)
+
+//Space Complexity: O(1)
+
+
+void bubble_sort(int arr[], int n) {
+  // bubble sort
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
