102e71032066c99fa897d5f06e60ca8bcbc6e53c
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2022-09-01 23:52:35 +0530

    Create 2.1 Selection_sort.cpp

---
+++ 2.Sorting_Techniques/2.1 Selection_sort.cpp
@@ -0,0 +1,12 @@
+for (int i = 0; i < n - 1; i++) {
+    int mini = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[mini]) {
+        mini = j;
+      }
+    }
+   swap(arr[i],arr[mini]);
+  }
+
+//TC:O(n^2)
+//SC: O(1)
