2772949e54784c0d66293bd4fd3475c8deb76124
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2022-09-02 13:01:10 +0530

    Create remove_duplicates.cpp

---
+++ 3.Arrays/3.1 Easy/remove_duplicates.cpp
@@ -0,0 +1,9 @@
+int remove_duplicate(int a[],int n){
+        int j=0;
+        for(int i=1;i<n;i++)
+        {
+            if(a[i]!=a[j]) j++;
+            a[j]=a[i];
+        }
+        return j+1;
+    }
